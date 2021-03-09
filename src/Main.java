eebd3f9c2a87f44c6dceb2a61c689b02317c833f
sevastyanov1982 <sevastyanov1982@gmail.com>
2021-03-09 22:18:56 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+     int ticket = 10000;
+     int miles = 1;
+     int spent = 20;
+     int bonus = ticket / spent * miles;
+        System.out.println("Начислено "+bonus +" миль");
+
+    }
+}
